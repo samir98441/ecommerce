43f78b3d64385219a5263a9210c7d13e08ad437b
SAMEER KAYASTHA <47190249+samir98441@users.noreply.github.com>
2021-06-23 23:46:14 +0545

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".one").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+    });
+    $(".four").owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        nav: true,
+    });
+});
